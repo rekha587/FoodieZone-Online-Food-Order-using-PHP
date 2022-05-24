d9360a586eaabd68f04e2f186153dd2487cc5d36
rekha587 <79393700+rekha587@users.noreply.github.com>
2022-05-24 20:35:08 +0530

    first commit

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+
+function Connect()
+{
+	$dbhost = "127.0.0.1:8111";
+	$dbuser = "root";
+	$dbpass = "";
+	$dbname = "foodorder";
+
+	//Create Connection
+	$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
+
+	return $conn;
+}
+?>
